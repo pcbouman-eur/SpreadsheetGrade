fc2b47adc084e30b90306a73cbedaaef98e35f14
Paul Bouman <bouman@ese.eur.nl>
2016-11-13 11:23:05 +0100

    Removed an unused import.

--- src/StreamGrader.java
+++ src/StreamGrader.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import javax.xml.bind.JAXBException;
 
